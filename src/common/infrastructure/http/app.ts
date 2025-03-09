39a747c1c90a1b2ba11c19c6aeb10d7cf8360284
GuiQRSM <quaresmagui20@gmail.com>
2025-03-08 22:04:07 -0300

    create the rror tratament

--- src/common/infrastructure/http/app.ts
+++ src/common/infrastructure/http/app.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 import cors from 'cors'
 import express from 'express'
+import { errorHandler } from './middlewares/errorHandler'
 import { routes } from './routes'
 
 const app = express()
@@ -8,5 +9,6 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use(errorHandler)
 
 export { app }
