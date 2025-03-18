85e5c13c4a72e25841e4d24c03faac903de4ae3c
GuiQRSM <quaresmagui20@gmail.com>
2025-03-18 00:09:56 -0300

    Add the 'Swagger' at the project

--- src/common/infrastructure/http/app.ts
+++ src/common/infrastructure/http/app.ts
@@ -1,13 +1,29 @@
 /* eslint-disable prettier/prettier */
 import cors from 'cors'
 import express from 'express'
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
 import { errorHandler } from './middlewares/errorHandler'
 import { routes } from './routes'
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+    },
+  },
+  apis: [],
+}
+
+const SwaggerSpec = swaggerJSDoc(options)
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(SwaggerSpec))
 app.use(routes)
 app.use(errorHandler)
 
--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -4,4 +4,5 @@ import { app } from './app'
 
 app.listen(env.PORT, () => {
   console.log(`Server is running on port ${env.PORT}`)
+  console.log('API docs is available at GET /docs')
 })
