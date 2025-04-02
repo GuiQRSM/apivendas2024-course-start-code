673a3e537711a928cd23a970848518fadddf6144
GuiQRSM <quaresmagui20@gmail.com>
2025-04-02 17:38:52 -0300

    Conecta o TypeORM com o Postgres

--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -1,8 +1,16 @@
 /* eslint-disable prettier/prettier */
 import { env } from '../env'
+import { dataSource } from '../typeorm'
 import { app } from './app'
 
-app.listen(env.PORT, () => {
-  console.log(`Server is running on port ${env.PORT}`)
-  console.log('API docs is available at GET /docs')
-})
+dataSource
+  .initialize()
+  .then(() => {
+    app.listen(env.PORT, () => {
+      console.log(`Server is running on port ${env.PORT}`)
+      console.log('API docs is available at GET /docs')
+    })
+  })
+  .catch(error => {
+    console.log('Error connecting to database', error)
+  })
