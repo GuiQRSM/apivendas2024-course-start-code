5e3738db9c3a4aab7d29235887ddf8c78cc4c91b
GuiQRSM <quaresmagui20@gmail.com>
2025-04-17 13:08:40 -0300

    Implement the class error

--- src/common/domain/repositories/in-memory.reposirory.ts
+++ src/common/domain/repositories/in-memory.reposirory.ts
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
 
+import { NotFoundError } from '../errors/not-found-error'
 import {
   RepositoriesInterface,
   SearchInput,
@@ -18,24 +19,38 @@ export type CreateProps = {
 export abstract class InMemoryRepository<Model extends ModelProps>
   implements RepositoriesInterface<Model, CreateProps>
 {
-  itens: Model[] = []
+  items: Model[] = []
   sortableFields: string[] = []
+
   create(props: CreateProps): Model {
     throw new Error('Method not implemented.')
   }
+
   insert(model: Model): Promise<Model> {
     throw new Error('Method not implemented.')
   }
-  findById(id: string): Promise<Model> {
-    throw new Error('Method not implemented.')
+
+  async findById(id: string): Promise<Model> {
+    return this._get(id)
   }
+
   update(model: Model): Promise<Model> {
     throw new Error('Method not implemented.')
   }
+
   delete(id: string): Promise<void> {
     throw new Error('Method not implemented.')
   }
+
   search(props: SearchInput): Promise<SearchOutput<Model>> {
     throw new Error('Method not implemented.')
+  }
+
+  protected async _get(id: string): Promise<Model> {
+    const model = this.items.find(item => item.id == id)
+    if (!model) {
+      throw new NotFoundError(`Model not found ID ${id}`)
+    }
+    return model
   }
 }
